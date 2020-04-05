89fa37df76395138b38606605f1f3df6aa63c392
jiaying-liu123 <62330645+jiaying-liu123@users.noreply.github.com>
2020-04-05 22:23:53 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,9 +1,35 @@
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 
-using namespace std;
-
+void maxsum(int n, int a[100])
+{
+	int i, max, sum=0;
+	max = a[0];
+	for (i = 0; i < n; i++)
+	{
+		if (sum> 0)
+			 sum= sum+a[i];
+		else
+			sum = a[i];
+		if (max < sum)
+			max = sum;
+	}
+	if (max < 0)
+	{
+		printf("0");
+	}
+	else
+	{
+		printf("%d", max);
+	}
+}
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+	int n, a[100],i;
+	scanf("%d", &n);
+	for (i = 0; i < n; i++)
+	{
+		scanf("%d", &a[i]);
+	}
+	maxsum(n, a);
 }
